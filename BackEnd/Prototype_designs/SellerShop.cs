3107289c2c2be2fd7e6d0e092d864287bb47f972
Kopil Kaiser <kopilkaiser24@gmail.com>
2021-02-05 14:25:40 +0000

    Created a folder named "Prototype_designs" in the Back_End project and moved the prototype designs in windows form

---
+++ BackEnd/Prototype_designs/SellerShop.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BackEnd
+{
+    public partial class SellerShop : Form
+    {
+        public SellerShop()
+        {
+            InitializeComponent();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SellerShop_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
